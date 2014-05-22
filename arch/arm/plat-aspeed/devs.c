60f3de948d0a10e20b9f88f7d2e06c5d6c16d6f9
Tian Fang <tfang@fb.com>
2014-05-22 16:36:48 -0700

    usb as module and disable uhci and sdhci

--- arch/arm/plat-aspeed/devs.c
+++ arch/arm/plat-aspeed/devs.c
@@ -6,11 +6,11 @@
 * This program is free software; you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by the Free Software Foundation;
 * either version 2 of the License, or (at your option) any later version.
-* 
+*
 * This program is distributed in the hope that it will be useful,  but WITHOUT ANY WARRANTY;
 * without even the implied warranty of MERCHANTABILITY or
 * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-* 
+*
 * You should have received a copy of the GNU General Public License
 * along with this program; if not, write to the Free Software
 * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
@@ -32,9 +32,9 @@ typedef void (init_fnc_t) (void);
 
 init_fnc_t __initdata *init_all_device[] = {
 	ast_add_device_uart,
-	ast_add_device_vuart,	
+	ast_add_device_vuart,
 	ast_add_device_watchdog,
-	ast_add_device_rtc,	
+	ast_add_device_rtc,
 	ast_add_device_i2c,
 	ast_add_device_ci2c,
 	ast_add_device_spi,
@@ -43,13 +43,13 @@ init_fnc_t __initdata *init_all_device[] = {
 	ast_add_device_flash,
 	ast_add_device_pwm_fan,
 	ast_add_device_adc,
-	ast_add_device_gpio,		
-	ast_add_device_sgpio,			
-	ast_add_device_peci,	
+	ast_add_device_gpio,
+	ast_add_device_sgpio,
+	ast_add_device_peci,
 	ast_add_device_fb,
-	ast_add_device_sdhci,
-	ast_add_device_uhci,
-	ast_add_device_video,		
+//	ast_add_device_sdhci, disabled by tfang
+//	ast_add_device_uhci, disabled by tfang
+//	ast_add_device_video, disabled by tfang
 	ast_add_device_cuart,
 	ast_add_device_kcs,
 	ast_add_device_mailbox,
