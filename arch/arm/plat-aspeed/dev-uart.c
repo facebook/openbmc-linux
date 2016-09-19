b330e8dfa6424ef7b0d994dca76f0be43b9f77ec
Jubin Mehta <jmehta@fb.com>
2016-09-19 00:40:01 -0700

    Config fbttn kernel uart setting
    Reviewed By: kapilh
    
    Differential Revision: D3881474
    
    fbshipit-source-id: 366e86a

--- arch/arm/plat-aspeed/dev-uart.c
+++ arch/arm/plat-aspeed/dev-uart.c
@@ -190,7 +190,7 @@ static struct plat_serial8250_port ast_uart_data[] = {
 		.flags	= UPF_IOREMAP | UPF_BOOT_AUTOCONF | UPF_SKIP_TEST,
 	},
 /* Without this, tty offset might change for others */
-#if defined(CONFIG_YOSEMITE) || defined(CONFIG_FBTP)
+#if defined(CONFIG_YOSEMITE) || defined(CONFIG_FBTP) || defined(CONFIG_FBTTN)
 	{
 		.mapbase	= AST_UART2_BASE,
 		.irq		= IRQ_UART2,
