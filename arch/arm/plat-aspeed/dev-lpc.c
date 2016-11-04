dcbc8822558891c1259f786ddd2769e8054dffcd
Tian Fang <tfang@fb.com>
2016-11-03 20:42:15 -0700

    enable UART1-4 when LPC is not enabled
    Reviewed By: sdasari73
    
    Differential Revision: D4136360
    
    fbshipit-source-id: ffb530b

--- arch/arm/plat-aspeed/dev-lpc.c
+++ arch/arm/plat-aspeed/dev-lpc.c
@@ -29,6 +29,7 @@
 #include <plat/devs.h>
 #include <plat/ast-scu.h>
 #include <plat/ast-lpc.h>
+#include <plat/regs-lpc.h>
 #include <mach/gpio.h>
 
 /* --------------------------------------------------------------------
@@ -123,5 +124,13 @@ void __init ast_add_device_lpc(void)
 #endif
 }
 #else
-void __init ast_add_device_lpc(void) {}
+void __init ast_add_device_lpc(void) {
+	// Since we disable LPC, bring the UART1 and UART2 out from LPC control
+	void __iomem *reg_base;
+	reg_base = ioremap(AST_LPC_BASE, SZ_256);
+	writel(readl(reg_base + AST_LPC_HICR9)
+	       & ~(LPC_HICR9_SOURCE_UART1|LPC_HICR9_SOURCE_UART2
+		   |LPC_HICR9_SOURCE_UART3|LPC_HICR9_SOURCE_UART4),
+	       reg_base + AST_LPC_HICR9);
+}
 #endif
