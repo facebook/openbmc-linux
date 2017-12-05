56bf2fd6df9c5a62f74a9e8a3b1095118843a32f
Mike Choi <mikechoi@fb.com>
2017-12-04 21:24:10 -0800

    Fix intermittent kernel crash in rmmod g_cdc
    Reviewed By: tfangit
    
    Differential Revision: D6483310
    
    fbshipit-source-id: 0a861ad

--- drivers/usb/gadget/composite.c
+++ drivers/usb/gadget/composite.c
@@ -1840,6 +1840,9 @@ void composite_disconnect(struct usb_gadget *gadget)
 	/* REVISIT:  should we have config and device level
 	 * disconnect callbacks?
 	 */
+  if (!cdev)
+    return;
+
 	spin_lock_irqsave(&cdev->lock, flags);
 	if (cdev->config)
 		reset_config(cdev);
