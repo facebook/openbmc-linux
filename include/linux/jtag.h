f7a0ef18316883e2d7ce39cc6b72b90a6a618c08
Howard Chiu <howard.chiu@quantatw.com>
2019-07-24 16:42:36 -0700

    kernel: Adding jtag master driver (#904)
    Summary:
    port from kernel 4.18 with commit #6c60449e7f646c3c59a7395da958c962373121fb
    Pull Request resolved: https://github.com/facebookexternal/openbmc.quanta/pull/904
    
    Test Plan:
    Build pass and test on fbep, kernel 5.0 (with Tiogapass system)
    
    root@bmc-oob:~# dmesg | grep jtag
    [   24.041717] ast_jtag: create sysfs device attributes.
    [   24.051979] ast_jtag: driver successfully loaded.
    
    root@bmc-oob:~# ls -la /dev/j*
    crw-------    1 root     root       10,  62 Dec 31  1969 /dev/jtag0
    
    root@bmc-oob:~# ls -la /sys/devices/platform/ahb/ahb\:apb/1e6e4000.jtag/
    drwxr-xr-x    2 root     root             0 Dec 31  1969 .
    drwxr-xr-x   17 root     root             0 Dec 31  1969 ..
    lrwxrwxrwx    1 root     root             0 Jul 22 18:25 driver -> ../../../../../bus/platform/drivers/jtag-aspeed
    -rw-r--r--    1 root     root          4096 Jul 22 18:25 driver_override
    -rw-r--r--    1 root     root          4096 Jul 22 18:25 freq
    -r--r--r--    1 root     root          4096 Jul 22 18:25 modalias
    lrwxrwxrwx    1 root     root             0 Jul 22 18:25 of_node -> ../../../../../firmware/devicetree/base/ahb/apb/jtag@1e6e4000
    -r--r--r--    1 root     root          4096 Jul 22 18:25 sts
    lrwxrwxrwx    1 root     root             0 Jul 22 18:25 subsystem -> ../../../../../bus/platform
    --w-------    1 root     root          4096 Jul 22 18:25 tck
    --w-------    1 root     root          4096 Jul 22 18:25 tdi
    -r--r--r--    1 root     root          4096 Jul 22 18:25 tdo
    --w-------    1 root     root          4096 Jul 22 18:25 tms
    -rw-r--r--    1 root     root          4096 Dec 31  1969 uevent
    
    Reviewed By: tomrepo
    
    Differential Revision: D16447345
    
    Pulled By: amithash
    
    fbshipit-source-id: 76701df3af

---
+++ include/linux/jtag.h
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: GPL-2.0
+// include/linux/jtag.h - JTAG class driver
+//
+// Copyright (c) 2017 Mellanox Technologies. All rights reserved.
+// Copyright (c) 2017 Oleksandr Shamray <oleksandrs@mellanox.com>
+
+#ifndef __JTAG_H
+#define __JTAG_H
+
+#include <uapi/linux/jtag.h>
+
+#define jtag_u64_to_ptr(arg) ((void *)(uintptr_t)arg)
+
+#define JTAG_MAX_XFER_DATA_LEN 65535
+
+struct jtag;
+/**
+ * struct jtag_ops - callbacks for jtag control functions:
+ *
+ * @freq_get: get frequency function. Filled by device driver
+ * @freq_set: set frequency function. Filled by device driver
+ * @status_get: set status function. Filled by device driver
+ * @idle: set JTAG to idle state function. Filled by device driver
+ * @xfer: send JTAG xfer function. Filled by device driver
+ */
+struct jtag_ops {
+    int (*freq_get)(struct jtag *jtag, u32 *freq);
+    int (*freq_set)(struct jtag *jtag, u32 freq);
+    int (*status_get)(struct jtag *jtag, u32 *state);
+    int (*idle)(struct jtag *jtag, struct jtag_run_test_idle *idle);
+    int (*xfer)(struct jtag *jtag, struct jtag_xfer *xfer, u8 *xfer_data);
+    int (*mode_set)(struct jtag *jtag, u32 mode_mask);
+};
+
+void *jtag_priv(struct jtag *jtag);
+int jtag_register(struct jtag *jtag);
+void jtag_unregister(struct jtag *jtag);
+struct jtag *jtag_alloc(size_t priv_size, const struct jtag_ops *ops);
+void jtag_free(struct jtag *jtag);
+
+#endif /* __JTAG_H */
+
