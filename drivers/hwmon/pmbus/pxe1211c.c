edfe08f73cf58ea2e9b296fdba6c90b562244d82
Tianhui Xu <tianhui@celestica.com>
2019-10-17 18:53:01 -0700

    hwmon: pmbus: add driver for Infineon PXE1121C (#177)
    Summary:
    Wedge400C has pxe1121c on EVT2, add pmbus drvier.
    Pull Request resolved: https://github.com/facebookexternal/openbmc.celestica/pull/177
    
    Test Plan:
    bitbake wedge400-image pass, after compiling:
    tianhui@openbmc-ubuntu16:~/facebook/openbmc.celestica/build$ ls tmp/work/wedge400-fb-linux-gnueabi/wedge400-image/1.0-r0/rootfs/lib/modules/5.0.3-wedge400/kernel/drivers/hwmon/pmbus/pxe1211c.ko
    tmp/work/wedge400-fb-linux-gnueabi/wedge400-image/1.0-r0/rootfs/lib/modules/5.0.3-wedge400/kernel/drivers/hwmon/pmbus/pxe1211c.ko
    As we don't have device to test this yet. Just compile pass.
    
    Pass
    
    Differential Revision: D17865584
    
    Pulled By: mikechoifb
    
    fbshipit-source-id: eaa5f32275

---
+++ drivers/hwmon/pmbus/pxe1211c.c
@@ -0,0 +1,132 @@
+/*
+ * pxe1211c.c - The i2c driver for pxe1211c
+ *
+ * Copyright 2019-present Facebook. All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#include <linux/err.h>
+#include <linux/i2c.h>
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include "pmbus.h"
+
+
+#define PXE1211C_NUM_PAGES	3
+
+
+/* Identify chip parameters. */
+static int pxe1211c_identify(struct i2c_client *client,
+							 struct pmbus_driver_info *info)
+{
+	if (pmbus_check_byte_register(client, 0, PMBUS_VOUT_MODE)) {
+		u8 vout_mode;
+		int ret;
+
+		/* Read the register with VOUT scaling value.*/
+		ret = pmbus_read_byte_data(client, 0, PMBUS_VOUT_MODE);
+		if (ret < 0)
+			return ret;
+
+		vout_mode = ret & GENMASK(4, 0);
+
+		switch (vout_mode) {
+		case 1:
+			info->vrm_version = vr12;
+			break;
+		case 2:
+			info->vrm_version = vr13;
+			break;
+		default:
+			return -ENODEV;
+		}
+	}
+
+	return 0;
+}
+
+static int pxe1211c_probe(struct i2c_client *client,
+						  const struct i2c_device_id *id)
+{
+	struct pmbus_driver_info *info;
+	u8 buf[I2C_SMBUS_BLOCK_MAX];
+	int ret;
+
+	if (!i2c_check_functionality(client->adapter,
+							  I2C_FUNC_SMBUS_READ_BYTE_DATA
+							| I2C_FUNC_SMBUS_READ_WORD_DATA
+							| I2C_FUNC_SMBUS_READ_BLOCK_DATA))
+		return -ENODEV;
+
+	ret = i2c_smbus_read_block_data(client, PMBUS_MFR_ID, buf);
+	if (ret < 0)
+	{
+		dev_err(&client->dev, "Failed to read PMBUS_MFR_ID\n");
+		return ret;
+	}
+	if (ret != 2 || strncmp(buf, "XP", strlen("XP")))
+	{
+		dev_err(&client->dev, "MFR_ID unrecognised\n");
+		return -ENODEV;
+	}
+
+	info = devm_kzalloc(&client->dev, sizeof(struct pmbus_driver_info),
+						GFP_KERNEL);
+	if (!info)
+		return -ENOMEM;
+
+	info->pages = PXE1211C_NUM_PAGES;
+	info->format[PSC_VOLTAGE_IN] = linear;
+	info->format[PSC_VOLTAGE_OUT] = vid;
+	info->format[PSC_CURRENT_IN] = linear;
+	info->format[PSC_CURRENT_OUT] = linear;
+	info->format[PSC_POWER] = linear;
+	info->format[PSC_TEMPERATURE] = linear;
+
+	info->func[0] = PMBUS_HAVE_VIN
+		| PMBUS_HAVE_VOUT | PMBUS_HAVE_IIN
+		| PMBUS_HAVE_IOUT | PMBUS_HAVE_PIN
+		| PMBUS_HAVE_POUT | PMBUS_HAVE_TEMP
+		| PMBUS_HAVE_STATUS_VOUT | PMBUS_HAVE_STATUS_IOUT
+		| PMBUS_HAVE_STATUS_INPUT | PMBUS_HAVE_STATUS_TEMP;
+	info->func[1] = info->func[0];
+	info->func[2] = info->func[0];
+	info->identify = pxe1211c_identify;
+
+	return pmbus_do_probe(client, id, info);
+}
+
+static const struct i2c_device_id pxe1211c_id[] = {
+	{"pxe1211c", 0},
+	{}};
+
+MODULE_DEVICE_TABLE(i2c, pxe1211c_id);
+
+static struct i2c_driver pxe1211c_driver = {
+	.driver = {
+		.name = "pxe1211c",
+	},
+	.probe = pxe1211c_probe,
+	.remove = pmbus_do_remove,
+	.id_table = pxe1211c_id,
+};
+
+module_i2c_driver(pxe1211c_driver);
+
+MODULE_AUTHOR("Kantaphon Kovathana <kkovath@celestica.com");
+MODULE_DESCRIPTION("PMBus driver for PXE1211C");
+MODULE_LICENSE("GPL");
