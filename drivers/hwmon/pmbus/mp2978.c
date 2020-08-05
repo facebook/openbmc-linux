e9dbfc06a30f143b9e831f543bf95a3f288fbc70
wxiaohua1217 <wxiaohua@celestica.com>
2020-08-05 11:35:06 -0700

    hwmon: (pmbus) add mp2978 driver (#538)
    Summary:
    MPS mp2978/mp2975 driver
    
    Pull Request resolved: https://github.com/facebookexternal/openbmc.celestica/pull/538
    
    Test Plan:
    root@bmc-oob:/sys/bus/i2c/devices/1-0053/hwmon/hwmon2# ls
    curr1_crit         curr2_crit         curr2_max          in1_input          in1_min            subsystem          temp1_max
    curr1_crit_alarm   curr2_crit_alarm   curr2_max_alarm    in1_label          in1_min_alarm      temp1_crit         temp1_max_alarm
    curr1_input        curr2_input        curr3_input        in1_lcrit          in2_input          temp1_crit_alarm   temp1_min
    curr1_label        curr2_label        device             in1_lcrit_alarm    in3_input          temp1_input        temp1_min_alarm
    curr1_max          curr2_lcrit        in1_crit           in1_max            name               temp1_lcrit        uevent
    curr1_max_alarm    curr2_lcrit_alarm  in1_crit_alarm     in1_max_alarm      power              temp1_lcrit_alarm
    root@bmc-oob:/sys/bus/i2c/devices/1-0053/hwmon/hwmon2# cat in1_input
    11750
    root@bmc-oob:/sys/bus/i2c/devices/1-0053/hwmon/hwmon2# cat in2_input
    751
    root@bmc-oob:/sys/bus/i2c/devices/1-0053/hwmon/hwmon2# cat in3_input
    1201
    root@bmc-oob:/sys/bus/i2c/devices/1-0053/hwmon/hwmon2# cat curr1_input
    2000
    root@bmc-oob:/sys/bus/i2c/devices/1-0053/hwmon/hwmon2# cat curr2_input
    17000
    root@bmc-oob:/sys/bus/i2c/devices/1-0053/hwmon/hwmon2# cat curr3_input
    8000
    root@bmc-oob:/sys/bus/i2c/devices/1-0053/hwmon/hwmon2# cat temp1_input
    55000
    
    Pass.
    
    Reviewed By: williamspatrick
    
    Differential Revision: D22801298
    
    Pulled By: mikechoifb
    
    fbshipit-source-id: a123b1a5d2

---
+++ drivers/hwmon/pmbus/mp2978.c
@@ -0,0 +1,124 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Hardware monitoring driver for MPS MP2978
+ *
+ * Copyright (c) 2020 Facebook Inc.
+ */
+
+#include <linux/err.h>
+#include <linux/hwmon-sysfs.h>
+#include <linux/i2c.h>
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include "pmbus.h"
+
+static ssize_t mp2978_vout_show(struct device *dev,
+				struct device_attribute *devattr,
+				char *buf)
+{
+	struct i2c_client *client = to_i2c_client(dev->parent);
+	struct sensor_device_attribute *attr = to_sensor_dev_attr(devattr);
+	int val = pmbus_read_word_data(client, attr->index, PMBUS_READ_VOUT);
+	return sprintf(buf, "%d\n", val);
+}
+
+static ssize_t mp2978_curr_out_show(struct device *dev,
+				struct device_attribute *devattr,
+				char *buf)
+{
+	struct i2c_client *client = to_i2c_client(dev->parent);
+	struct sensor_device_attribute *attr = to_sensor_dev_attr(devattr);
+	int val = pmbus_read_word_data(client, attr->index, PMBUS_READ_IOUT);
+	return sprintf(buf, "%d\n", val * 1000);
+}
+
+static ssize_t mp2978_vout_store(struct device *dev,
+				struct device_attribute *devattr,
+				const char *buf, size_t count)
+{
+	return -1;
+}
+
+static ssize_t mp2978_curr_out_store(struct device *dev,
+				struct device_attribute *devattr,
+				const char *buf, size_t count)
+{
+	return -1;
+}
+
+/*
+ * There are two rail voltage need monitored by MP2975/MP2975, rail 1 can read
+ * from Page0 and rail 2 Rail 2 need read from Page1.
+ * But feedback from MPS FAE the MP2978/2975 chip have two issues:
+ * 1. page0 vout mode is VID, but the vout_mode of page1 default is linear.
+ * 2. page0 vout mode is VID, but vout vout report mode is direct format.
+ */
+static SENSOR_DEVICE_ATTR_RW(in2_input, mp2978_vout, 0);
+static SENSOR_DEVICE_ATTR_RW(in3_input, mp2978_vout, 1);
+static SENSOR_DEVICE_ATTR_RW(curr3_input, mp2978_curr_out, 1);
+
+static struct attribute *enable_attrs[] = {
+	&sensor_dev_attr_in2_input.dev_attr.attr,
+	&sensor_dev_attr_in3_input.dev_attr.attr,
+	&sensor_dev_attr_curr3_input.dev_attr.attr,
+	NULL,
+};
+
+static const struct attribute_group enable_group = {
+	.attrs = enable_attrs,
+};
+
+static const struct attribute_group *attribute_groups[] = {
+	&enable_group,
+	NULL,
+};
+
+/*
+ * MP2978/MP2975 support input voltage, output voltage, output current, temperature
+ * monitor.
+ */
+static struct pmbus_driver_info mp2978_info = {
+	.pages = 1,
+	.format[PSC_VOLTAGE_IN] = linear,
+	.format[PSC_VOLTAGE_OUT] = vid,
+	.vrm_version[0] = vr12,
+	.format[PSC_CURRENT_IN] = linear,
+	.format[PSC_CURRENT_OUT] = linear,
+	.format[PSC_TEMPERATURE] = linear,
+	.func[0] = PMBUS_HAVE_TEMP
+		| PMBUS_HAVE_VIN
+		| PMBUS_HAVE_IIN | PMBUS_HAVE_IOUT
+		| PMBUS_HAVE_STATUS_IOUT
+		| PMBUS_HAVE_STATUS_INPUT | PMBUS_HAVE_STATUS_TEMP,
+	.groups = attribute_groups,
+};
+
+static int mp2978_probe(struct i2c_client *client,
+			 const struct i2c_device_id *id)
+{
+	return pmbus_do_probe(client, id, &mp2978_info);
+}
+
+static const struct i2c_device_id mp2978_id[] = {
+	{"mp2978", 0},
+	{"mp2975", 0},
+	{}
+};
+
+MODULE_DEVICE_TABLE(i2c, mp2978_id);
+
+static struct i2c_driver mp2978_driver = {
+	.driver = {
+		   .name = "mp2978",
+		   },
+	.probe = mp2978_probe,
+	.remove = pmbus_do_remove,
+	.id_table = mp2978_id,
+};
+
+module_i2c_driver(mp2978_driver);
+
+MODULE_AUTHOR("Facebook/Celestica");
+MODULE_DESCRIPTION("PMBus driver for MPS MP2978/MP2975");
+MODULE_LICENSE("GPL");
