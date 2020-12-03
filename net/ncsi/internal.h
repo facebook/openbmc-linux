f1171653b50c5d6b1d4a0ff6f95f63ecdf28f174
Ben Wei <benwei@fb.com>
2020-12-03 10:22:46 -0800

    linux 5.6/ncsi: add 3x retry for DCNT cmd
    Reviewed By: tao-ren
    
    Differential Revision: D25253671
    
    fbshipit-source-id: 713106fd0e

--- net/ncsi/internal.h
+++ net/ncsi/internal.h
@@ -310,6 +310,8 @@ struct ncsi_dev_priv {
 #define NCSI_DEV_HWA		2            /* Enabled HW arbitration     */
 #define NCSI_DEV_RESHUFFLE	4
 #define NCSI_DEV_RESET		8            /* Reset state of NC          */
+#define NCSI_CMD_RETRY_MAX      3
+	unsigned int        cmd_retry;       /* Retry command              */
 	unsigned int        gma_flag;        /* OEM GMA flag               */
 	spinlock_t          lock;            /* Protect the NCSI device    */
 	unsigned int        package_probe_id;/* Current ID during probe    */
