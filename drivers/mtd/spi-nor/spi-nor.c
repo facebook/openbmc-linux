c0be87af5c6fef045973311930b28583ce2f7e62
Jerry C Chen <Jerry_C_Chen@wiwynn.com>
2021-04-27 12:09:11 -0700

    kernel 5.6: support SPI flash of w25q01jv-im (#1572)
    Summary:
    Support SPI flash of w25q01jv-im.
    
    Pull Request resolved: https://github.com/facebookexternal/openbmc.wiwynn/pull/1572
    
    Test Plan:
    Build and test pass on Grand Canyon.
    
    1. Check kernel message from spi-nor driver.
    
    root@bmc-oob:~# dmesg | grep w25q
    [   23.349548] aspeed-smc 1e620000.spi: w25q01jv-im (131072 Kbytes)
    [   23.607808] aspeed-smc 1e620000.spi: w25q01jv-im (131072 Kbytes)
    
    2. Check /proc/mtd is correct.
    
    root@bmc-oob:~# cat /proc/mtd
    dev:    size   erasesize  name
    mtd0: 00040000 00001000 "romx"
    mtd1: 00010000 00001000 "env"
    mtd2: 00010000 00001000 "meta"
    mtd3: 000a0000 00001000 "u-boot"
    mtd4: 01e60000 00001000 "fit"
    mtd5: 00400000 00001000 "data0"
    mtd6: 08000000 00001000 "flash1"
    mtd7: 07ff0000 00001000 "flash1rw"
    mtd8: 00040000 00001000 "rom"
    mtd9: 000a0000 00001000 "recovery"
    mtd10: 00010000 00001000 "metaro"
    mtd11: 01e60000 00001000 "fitro"
    mtd12: 02000000 00001000 "flash0"
    
    3. Check there's no abnormal log in BMC /var/log/messages.
    
    Reviewed By: benwei13
    
    Differential Revision: D28032737
    
    Pulled By: zhdaniel12
    
    fbshipit-source-id: f12ac76ddb

--- drivers/mtd/spi-nor/spi-nor.c
+++ drivers/mtd/spi-nor/spi-nor.c
@@ -2690,6 +2690,7 @@ static const struct flash_info spi_nor_ids[] = {
 			SECT_4K | SPI_NOR_QUAD_READ | SPI_NOR_DUAL_READ) },
 	{ "w25q512jv", INFO(0xef4020, 0, 64 * 1024, 1024, SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ |
 			SPI_NOR_HAS_LOCK | SPI_NOR_HAS_TB) },
+	{ "w25q01jv-im", INFO(0xef7021, 0, 64 * 1024, 2048, SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ) },
 	{ "w25q01jv-iq", INFO(0xef4021, 0, 64 * 1024, 2048, SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ) },
 
 	/* Catalyst / On Semiconductor -- non-JEDEC */
