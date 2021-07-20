f32daee458e3f374c85fefef575524e87887140d
wxiaohua1217 <wxiaohua@celestica.com>
2021-07-20 12:38:31 -0700

    mtd: spi-nor: winbond: add w25q01jvtb support (#1151)
    Summary:
    Coludripper use the w25q01jvtb spi-nor flash as the second source flash. So need add w25q01jvtb flash in the winbond driver.
    
    Pull Request resolved: https://github.com/facebookexternal/openbmc.celestica/pull/1151
    
    Test Plan:
    ```
    root@bmc-oob:/mnt/data1/Images/BMC# flashcp /mnt/data1/Images/BMC/flash-cloudripper-v1.00 /dev/mtd5 -v
    Erasing block: 5980/5980 (100%)
    Writing kb: 23916/23916 (100%)
    Verifying kb: 23916/23916 (100%)
    root@bmc-oob:/mnt/data1/Images/BMC#
    
    ```
    
    Reviewed By: benwei13
    
    Differential Revision: D29609281
    
    Pulled By: tao-ren
    
    fbshipit-source-id: 26318a7c28

--- drivers/mtd/spi-nor/winbond.c
+++ drivers/mtd/spi-nor/winbond.c
@@ -99,6 +99,8 @@ static const struct flash_info winbond_parts[] = {
 			SPI_NOR_HAS_LOCK | SPI_NOR_HAS_TB) },
 	{ "w25m512jv", INFO(0xef7119, 0, 64 * 1024, 1024,
 			    SECT_4K | SPI_NOR_QUAD_READ | SPI_NOR_DUAL_READ) },
+	{ "w25q01jvtb", INFO(0xef4021, 0, 64 * 1024, 2048, SECT_4K | SPI_NOR_DUAL_READ |
+			    SPI_NOR_QUAD_READ | SPI_NOR_HAS_LOCK | SPI_NOR_HAS_TB) },
 };
 
 /**
