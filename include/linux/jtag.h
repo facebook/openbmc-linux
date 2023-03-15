f8f48d1cad30e3c3838094c26b4c91f1d82ee70e
Ernesto Corona <ernesto.corona@intel.com>
2023-03-15 15:56:58 -0700

    ASD Add Shift IR/DR from Exit IR/DR for HW2 JTAG xfers (#3677)
    Summary:
    Before this change JTAG HW2 mode shift operation failed to process a shift request when current state was Exit IR/DR for the same type of xfer SHIFTIR/SHIFTDR respectively. After this change we will support shift operations in HW2 mode from the following jtag states:
    
    For SHIFTDR: JTAG_STATE_SHIFTDR, JTAG_STATE_IDLE, JTAG_STATE_TLRESET,
                 JTAG_STATE_PAUSEDR, JTAG_STATE_EXIT1DR and
                 JTAG_STATE_EXIT1IR
    
    For SHIFTIR: JTAG_STATE_SHIFTIR, JTAG_STATE_IDLE, JTAG_STATE_TLRESET,
                 JTAG_STATE_PAUSEIR, JTAG_STATE_EXIT1IR and
                 JTAG_STATE_EXIT1DR
    
    X-link: https://github.com/facebookexternal/openbmc.quanta/pull/3677
    
    Reviewed By: williamspatrick
    
    Differential Revision: D44099801
    
    Pulled By: amithash
    
    fbshipit-source-id: 467129a745467a12c77c91db457aa4b1394b0fb0

--- include/linux/jtag.h
+++ include/linux/jtag.h
@@ -1,11 +1,10 @@
 /* SPDX-License-Identifier: GPL-2.0 */
-// include/linux/jtag.h - JTAG class driver
-//
-// Copyright (c) 2018 Mellanox Technologies. All rights reserved.
-// Copyright (c) 2018 Oleksandr Shamray <oleksandrs@mellanox.com>
+/* Copyright (c) 2018 Mellanox Technologies. All rights reserved. */
+/* Copyright (c) 2018 Oleksandr Shamray <oleksandrs@mellanox.com> */
+/* Copyright (c) 2019 Intel Corporation */
 
-#ifndef __JTAG_H
-#define __JTAG_H
+#ifndef __LINUX_JTAG_H
+#define __LINUX_JTAG_H
 
 #include <linux/types.h>
 #include <uapi/linux/jtag.h>
@@ -22,19 +21,21 @@ struct jtag;
  * @status_set: set JTAG TAPC state function. Mandatory, Filled by dev driver
  * @xfer: send JTAG xfer function. Mandatory func. Filled by dev driver
  * @mode_set: set specific work mode for JTAG. Filled by dev driver
- * @ioctl: handle driver specific ioctl requests. Filled by dev driver
+ * @bitbang: set low level bitbang operations. Filled by dev driver
+ * @enable: enables JTAG interface in master mode. Filled by dev driver
+ * @disable: disables JTAG interface master mode. Filled by dev driver
  */
 struct jtag_ops {
 	int (*freq_get)(struct jtag *jtag, u32 *freq);
 	int (*freq_set)(struct jtag *jtag, u32 freq);
 	int (*status_get)(struct jtag *jtag, u32 *state);
-	int (*status_set)(struct jtag *jtag, struct jtag_end_tap_state *endst);
+	int (*status_set)(struct jtag *jtag, struct jtag_tap_state *endst);
 	int (*xfer)(struct jtag *jtag, struct jtag_xfer *xfer, u8 *xfer_data);
 	int (*mode_set)(struct jtag *jtag, struct jtag_mode *jtag_mode);
-	int (*bitbang)(struct jtag *jtag, struct tck_bitbang *tck_bitbang);
+	int (*bitbang)(struct jtag *jtag, struct bitbang_packet *bitbang,
+		       struct tck_bitbang *bitbang_data);
 	int (*enable)(struct jtag *jtag);
 	int (*disable)(struct jtag *jtag);
-	int (*ioctl)(struct jtag *jtag, unsigned int cmd, unsigned long arg);
 };
 
 void *jtag_priv(struct jtag *jtag);
@@ -43,4 +44,4 @@ struct jtag *jtag_alloc(struct device *host, size_t priv_size,
 			const struct jtag_ops *ops);
 void jtag_free(struct jtag *jtag);
 
-#endif /* __JTAG_H */
+#endif /* __LINUX_JTAG_H */
