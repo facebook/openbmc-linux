97bee8bede3f244dadadc15bb1cddae1d8c6ca07
Ryan <ryan.hsu.wiwynn@gmail.com>
2026-08-26 13:32:44 -0700

    kernel 5.10: support Macronix MX77U51255G flash
    Summary:
    # Description:
    Add Macronix MX77U51255G info to the SPI NOR flash info table so that the spi-nor driver can successfully probe this flash chip.
    
    # Motivation:
    Enable secondary BIOS flash support on the netlakenext platform.
    
    X-link: https://github.com/facebookexternal/openbmc.wiwynn/pull/3688
    
    Reviewed By: amithash
    
    Differential Revision: D117325723
    
    fbshipit-source-id: 93f68d7953a8aabd53fdfb8c6130643d49b5872f

--- drivers/mtd/spi-nor/macronix.c
+++ drivers/mtd/spi-nor/macronix.c
@@ -90,6 +90,9 @@ static const struct flash_info macronix_parts[] = {
 	{ "mx66u2g45g",	 INFO(0xc2253c, 0, 64 * 1024, 4096,
 			      SECT_4K | SPI_NOR_DUAL_READ |
 			      SPI_NOR_QUAD_READ | SPI_NOR_4B_OPCODES) },
+	{ "mx77u51255g", INFO(0xc2763a, 0, 64 * 1024, 1024,
+				  SECT_4K | SPI_NOR_DUAL_READ |
+				  SPI_NOR_QUAD_READ | SPI_NOR_4B_OPCODES) },
 };
 
 static void macronix_default_init(struct spi_nor *nor)
